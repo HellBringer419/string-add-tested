fc055924e87c1c68a5a29049c3733a50808aa39d
Hell Bringer 419 <35007817+HellBringer419@users.noreply.github.com>
2021-11-11 18:54:25 +0530

    failed when testing add()

--- string_calculator.test.py
+++ string_calculator.test.py
@@ -5,6 +5,9 @@ class FirstTest(unittest.TestCase):
 
     def test_hello(self):
         self.assertEqual(StringCalculator.hello_world(), 'Hello World')
+    
+    def test_add(self):
+        self.assertEqual(StringCalculator.add("1,2"), 3)
 
 if __name__ == '__main__':
     unittest.main()
