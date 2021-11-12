684719581d9eb135406a253d564fda50136405d0
Hell Bringer 419 <35007817+HellBringer419@users.noreply.github.com>
2021-11-12 09:03:40 +0530

    failed when switching code structure

--- string_calculator.py
+++ string_calculator.py
@@ -1,10 +1,10 @@
 class StringCalculator():
     def add(numbers: str) -> int:
-        numbersArray = [ int(number) for number in numbers.split(',')]
+        numbersArray = numbers.split(',')
         number1 = 0
         number2 = 0
         if numbersArray.__len__() == 2:
-            [number1, number2] = numbersArray
+            [number1, number2] = [ int(number) for number in numbersArray]
         if numbersArray.__len__() == 1:
             [number1] = numbersArray
         return number1 + number2
