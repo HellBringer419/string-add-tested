cc76d028e54fe249aa5e5b4cc4dc3ad3c466711c
Hell Bringer 419 <35007817+HellBringer419@users.noreply.github.com>
2021-11-12 08:40:34 +0530

    failed when adding only 1 number

--- string_calculator_test.py
+++ string_calculator_test.py
@@ -3,11 +3,11 @@ from string_calculator import StringCalculator
 
 class FirstTest(unittest.TestCase):
 
-    def test_hello(self):
-        self.assertEqual(StringCalculator.hello_world(), 'Hello World')
-    
     def test_add(self):
         self.assertEqual(StringCalculator.add("1,2"), 3)
+    
+    def test_add_1(self):
+        self.assertEqual(StringCalculator.add("1"), 1)
 
 if __name__ == '__main__':
     unittest.main()
