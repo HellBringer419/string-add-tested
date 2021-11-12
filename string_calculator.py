c7c17a2dc63a29e59ca838ed121d917c089aada5
Hell Bringer 419 <35007817+HellBringer419@users.noreply.github.com>
2021-11-12 09:07:03 +0530

    passing when adding 0 numbers

--- string_calculator.py
+++ string_calculator.py
@@ -5,8 +5,10 @@ class StringCalculator():
         number2 = 0
         if numbersArray.__len__() == 2:
             [number1, number2] = [ int(number) for number in numbersArray]
-        if numbersArray.__len__() == 1:
+        elif numbersArray.__len__() == 1:
             [number1] = numbersArray
             if number1 == '':
                 number1 = 0
+            else:
+                number1 = int(number1)
         return number1 + number2
