3ca4f8e92eff2dfe9693d66e81b460a50e399be2
Hell Bringer 419 <35007817+HellBringer419@users.noreply.github.com>
2021-11-12 08:55:06 +0530

    failing when adding 0 numbers

--- string_calculator_test.py
+++ string_calculator_test.py
@@ -8,6 +8,9 @@ class FirstTest(unittest.TestCase):
     
     def test_add_1(self):
         self.assertEqual(StringCalculator.add("1"), 1)
+    
+    def test_add_0(self):
+        self.assertEqual(StringCalculator.add(""), 0)
 
 if __name__ == '__main__':
     unittest.main()
