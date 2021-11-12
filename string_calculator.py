e500b3352550125a9123d3c959760876aec5beba
Hell Bringer 419 <35007817+HellBringer419@users.noreply.github.com>
2021-11-12 08:51:04 +0530

    failing when switching the way code is written

--- string_calculator.py
+++ string_calculator.py
@@ -3,5 +3,9 @@ class StringCalculator():
         return "Hello World"
 
     def add(numbers: str) -> int:
-        [number1, number2] = [ int(number) for number in numbers.split(',')]
+        numbersArray = [ int(number) for number in numbers.split(',')]
+        number1 = 0
+        number2 = 0
+        if numbersArray.__len__() == 2:
+            [number1, number2] = numbersArray
         return number1 + number2
