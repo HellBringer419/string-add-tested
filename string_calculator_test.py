5e120a46543229ee8ea33ad8e7e034dc09672a38
Hell Bringer 419 <35007817+HellBringer419@users.noreply.github.com>
2021-11-14 18:33:09 +0530

    failing when adding unknown amount of numbers

--- string_calculator_test.py
+++ string_calculator_test.py
@@ -1,4 +1,5 @@
 import unittest
+from random import randint
 from string_calculator import StringCalculator
 
 class FirstTest(unittest.TestCase):
@@ -11,6 +12,25 @@ class FirstTest(unittest.TestCase):
     
     def test_add_0(self):
         self.assertEqual(StringCalculator.add(""), 0)
+
+class SecondTest(unittest.TestCase):
+
+    def test_add_n(self):
+        # To simulate unknown amount of numbers, we generate a random whole number
+        # then used that to pass arguments as "0, 1, ... (n-1)" to add function
+        # random_number = randint(0, 500)
+        random_number = 2
+        argument_string = ""
+        for i in range(0, random_number):
+            if(i == random_number-1):
+                argument_string += str(i)
+            else:
+                argument_string += str(i) + ","
+        
+        ## sum of (n-1) natural numbers: n * (n-1) / 2
+        expected_result = int(random_number * (random_number - 1) / 2)
+
+        self.assertEqual(StringCalculator.add(argument_string), expected_result)
 
 if __name__ == '__main__':
     unittest.main()
