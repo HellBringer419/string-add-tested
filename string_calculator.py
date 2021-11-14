b537ab9ad8ecd35d743bf571a82a0f3dcd23ad9a
Hell Bringer 419 <35007817+HellBringer419@users.noreply.github.com>
2021-11-14 18:34:26 +0530

    passing when adding unknown amount of numbers

--- string_calculator.py
+++ string_calculator.py
@@ -1,14 +1,10 @@
 class StringCalculator():
     def add(numbers: str) -> int:
-        numbersArray = numbers.split(',')
-        number1 = 0
-        number2 = 0
-        if numbersArray.__len__() == 2:
-            [number1, number2] = [ int(number) for number in numbersArray]
-        elif numbersArray.__len__() == 1:
-            [number1] = numbersArray
-            if number1 == '':
-                number1 = 0
-            else:
-                number1 = int(number1)
-        return number1 + number2
+        if numbers == '':
+            return 0
+        else: 
+            numbersArray = [ int(number) for number in numbers.split(',')]
+            sum = 0
+            for number in numbersArray:
+                sum += number
+            return sum
