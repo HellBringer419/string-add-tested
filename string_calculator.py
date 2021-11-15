5c49eea84c3947ae7e931823d9f4fcfe7a9e1b45
Hell Bringer 419 <35007817+HellBringer419@users.noreply.github.com>
2021-11-15 09:46:25 +0530

    passing when adding /n as param

--- string_calculator.py
+++ string_calculator.py
@@ -1,9 +1,23 @@
 class StringCalculator():
     def add(numbers: str) -> int:
-        if numbers == '':
+        if numbers is '':
             return 0
         else:
-            numbersArray = [int(number) for number in numbers.split(',')]
+            # splitting manually
+            # Keep a buffer to store all characters before delimiters 
+            # have a list to collect all numbers (after splitting using dilimiters)
+            buffer = ''
+            numbersArray = list()
+            for char in numbers:
+                if char is ',' or char is '\n':
+                    numbersArray.append(int(buffer))
+                    buffer = ''
+                else:
+                    buffer += char
+            
+            # flush all remaining in buffer to the array
+            if buffer is not '':
+                numbersArray.append(int(buffer))
             sum = 0
             for number in numbersArray:
                 sum += number
